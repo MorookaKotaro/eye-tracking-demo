16854099693f63e80bf9a53721aef68f1af43f71
MorookaKotaro <morookakotaro@team-lab.com>
2021-12-03 23:12:40 +0900

    トトロ設置

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,10 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
-import objModel from 'url:/static/plant.obj';
-import mtl from 'url:/static/plant.mtl';
+import glbModel from 'url:/static/Totoro.glb';
 
 let camera, scene, renderer;
 let geometry, material, mesh;
@@ -26,50 +23,75 @@ export default class Sketch {
     this.renderer = new THREE.WebGLRenderer( { antialias: true, canvas: canvas } );
     this.renderer.setSize( window.innerWidth, window.innerHeight );
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    this.renderer.shadowMap.enabled = true;
 
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-    this.camera.position.x = 1
-    this.camera.position.y = 1
-    this.camera.position.z = 2
+    this.camera.position.y = 0.5;
+    this.camera.position.z = 3
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     this.scene.add(this.camera)
 
-    this.controls = new OrbitControls(this.camera, canvas)
-    this.controls.enableDamping = true
+    // this.controls = new OrbitControls(this.camera, canvas)
+    // this.controls.enableDamping = true
   
 
     this.light = new THREE.AmbientLight(0x404040);
     this.scene.add(this.light);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7)
-    directionalLight.position.set(2, 2, - 1)
-
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.castShadow = true;
+    directionalLight.position.set(2, 2, 1);
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
     this.scene.add(directionalLight)
-    
-    const mtlLoader = new MTLLoader();
 
-    mtlLoader.load(mtl, (material) => {
-      material.preload();
+    const dHelper = new THREE.DirectionalLightHelper(directionalLight);
+    this.scene.add(dHelper)
 
-      const loader = new OBJLoader();
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+    this.scene.add(ambientLight)
 
-      // loader.setMaterials(material);
 
-      loader.load(objModel, (model) => {
-        model.position.set(0, 0, 0)
-        model.scale.set(0.002, 0.002, 0.002)
-        this.scene.add(model);
-      })
+    const loader = new GLTFLoader();
 
+    loader.load(glbModel, (model) => {
+      model.scene.position.set(0, -0.5, 0)
+      model.scene.scale.set(0.08, 0.08, 0.08)
+      model.scene.traverse( ( node ) => {
+        node.castShadow = true;
+      } );
+      // model.scene.castShadow = true;
+      this.scene.add(model.scene);
     })
+
+    const planeGeometry = new THREE.PlaneBufferGeometry(10, 10);
+    const planeMaterial = new THREE.MeshStandardMaterial({color: '#dddddd'});
+
+    const plane1 = new THREE.Mesh(
+      planeGeometry,
+      planeMaterial,
+    )
+    plane1.receiveShadow = true;
+    plane1.position.set(0, 0.8, -1);
+
+    const plane2 = new THREE.Mesh(
+      planeGeometry,
+      planeMaterial,
+    )
+    plane2.receiveShadow = true;
+    plane2.rotateX(-Math.PI / 2)
+    plane2.position.set(0, -0.5, 0);
+
+    this.scene.add(plane1, plane2);
 
     this.render();
   }
 
   render() {
     this.renderer.render( this.scene, this.camera );
-    this.controls.update()
+    // this.controls.update()
     window.requestAnimationFrame(this.render.bind(this));
   }
 }
